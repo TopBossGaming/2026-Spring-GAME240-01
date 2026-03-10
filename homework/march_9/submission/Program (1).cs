49e0214a391da29a9db0fccb48013f952b2769fb
TopBossGaming <ninjadiego121@gmail.com>
2026-03-10 16:30:07 -0700

    Add files via upload

---
+++ homework/march_9/submission/Program (1).cs
@@ -0,0 +1,33 @@
+﻿//using a while loop, find the biggest and smallest numbers in the array.
+
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+int[] array = [43, -5, 95, 612, 0];
+
+int i;
+i = 0;
+int biggestNum;
+int smallestNum;
+
+biggestNum = array[i];
+smallestNum = array[i];
+
+while (i < array.Length)
+{
+    int currentNumber = array[i];
+
+    if (currentNumber > biggestNum)
+    {
+        biggestNum = currentNumber;
+    }
+    if (currentNumber < smallestNum)
+    {
+        smallestNum = currentNumber;
+    }
+    i = i + 1;
+    
+}
+
+Console.WriteLine("Your smallest number is " + smallestNum);
+Console.WriteLine("Your biggest number is " + biggestNum);
